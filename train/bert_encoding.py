ec30f0bd20a5083875dd4a2cdcbd01b012c835d2
MUYUAN ZHANG <muyuanzhang14@gmail.com>
2025-05-10 22:22:37 -0400

    Update bert_encoding.py

--- train/bert_encoding.py
+++ train/bert_encoding.py
@@ -17,7 +17,7 @@ AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY", "hrwbqzUS85G253y
 EXPERIMENT_NAME = "bert-track-embeddings"
 
 # Data paths
-DATA_DIR = os.environ.get("PLAYLIST_DATA_DIR", "/mnt/object/processed")
+DATA_DIR = os.environ.get("PLAYLIST_DATA_DIR", "~/processed_data")
 TRACK_JSON_PATH = os.path.join(DATA_DIR, "track_texts.json")  # JSON file with track information
 
 # Model parameters
